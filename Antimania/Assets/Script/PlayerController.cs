03a38bf111baecb422c86798a0ee901b34772f34
Steven <steven.hgng@gmail.com>
2018-01-17 16:56:20 +0100

    implemented a basic physical working scene

---
+++ Antimania/Assets/Script/PlayerController.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+    
+    public static PlayerController instance;
+    int score;
+
+    void Awake()
+    {
+        // If we don't currently have a game controll....
+        if (instance == null)
+        {
+            instance = this;
+        }
+        else if (instance != this)
+            Destroy(gameObject);
+        DontDestroyOnLoad(this); // this object needs to be kept throughout the wholegame
+    }
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void Die() 
+    {
+        Debug.Log("Game over!");
+    }
+
+}
